98331718019c7625d5a1c947384b51ee87620ddd
Goh Bock Yang <bockyanggoh@gmail.com>
2019-12-15 02:35:18 +0800

    updated script for post provision steps

--- azure-provision/virtual-machine/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#az group create -l 'southeast asia' -n 'kafka-rg'
-az group deployment create --resource-group 'kafka-rg' --template-file kafka-vm-deploy.json --parameters kafka-vm-parameters.json
