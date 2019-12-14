6d96d684019b597b1765433ed3b4796697f3e352
Goh Bock Yang <bockyanggoh@gmail.com>
2019-12-15 03:19:28 +0800

    added zookeeper docker config

--- azure-provision/postinstall-scripts/install.sh
+++ azure-provision/postinstall-scripts/install.sh
@@ -15,3 +15,8 @@ sudo usermod -aG docker $(whoami)
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Download Kafka Workshop files
+git clone https://github.com/bockyanggoh/kafka-workshop.git ~/kafka-workshop
+
+# Set Hostname in ENV
