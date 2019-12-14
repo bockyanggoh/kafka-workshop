33c7d1ee2fef5d1d03e433bcd716c99bbfc77d8c
Goh Bock Yang <bockyanggoh@gmail.com>
2019-12-15 03:54:26 +0800

    fixing issue where ZK_O_HOSTNAME was causing warnings

--- docker-scripts/1-zookeeper/run.sh
+++ docker-scripts/1-zookeeper/run.sh
@@ -2,10 +2,10 @@
 
 compose_file="$1"
 other_ip="$2"
-docker-compose -f $compose_file down
 export ZK_O_HOSTNAME=$other_ip
-
 echo "ZK_O_HOSTNAME is set to $ZK_O_HOSTNAME"
+
+docker-compose -f $compose_file down
 docker-compose -f $compose_file up -d
 
 
