8d8ff0f36d0a931f529edb0fe4100141277eef19
Goh Bock Yang <bockyanggoh@gmail.com>
2019-12-15 03:04:28 +0800

    adding nohup to kafka1 setup to run kafka2 setup concurrently

--- azure-provision/run.sh
+++ azure-provision/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 az group create -l 'southeast asia' -n 'kafka-rg'
 az group deployment create --resource-group 'kafka-rg' --template-file azure-provision/kafka-vm-deploy.json --parameters azure-provision/kafka-vm-parameters.json
-az vm run-command invoke -g 'kafka-rg' -n 'kafka1' --command-id RunShellScript --scripts @azure-provision/postinstall-scripts/install.sh
+nohup az vm run-command invoke -g 'kafka-rg' -n 'kafka1' --command-id RunShellScript --scripts @azure-provision/postinstall-scripts/install.sh &
 az vm run-command invoke -g 'kafka-rg' -n 'kafka2' --command-id RunShellScript --scripts @azure-provision/postinstall-scripts/install.sh
 
 echo "Mission Complete. Keep the following information!"
