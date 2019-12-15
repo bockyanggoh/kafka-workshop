e6d2de92641f6e916711a52ce05c2ad76a18b269
Goh Bock Yang <bockyanggoh@gmail.com>
2019-12-15 17:07:56 +0800

    added java installation to post install, fixed issue where kafka cli was not working

--- azure-provision/postinstall-scripts/install.sh
+++ azure-provision/postinstall-scripts/install.sh
@@ -1,9 +1,10 @@
 # Install useful tools for networking checks
-sudo yum check-update
+sudo yum update -y
 sudo yum install nc -y
 sudo yum install git -y
 sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
 sudo yum install htop -y
+sudo yum install java-11-openjdk-devel -y
 # Install Docker
 curl -fsSL https://get.docker.com/ | sh
 sudo systemctl start docker
@@ -19,10 +20,10 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # Install Kafka CLI
-wget https://www-eu.apache.org/dist/kafka/2.3.0/kafka_2.12-2.3.0.tgz
-tar -xzf kafka_2.12-2.3.0.tgz
-export PATH=$PATH:$(pwd)/kafka_2.12-2.3.0/bin
-
+wget --directory-prefix=/tmp/ https://www-eu.apache.org/dist/kafka/2.3.0/kafka_2.12-2.3.0.tgz
+tar -C /tmp -xzf /tmp/kafka_2.12-2.3.0.tgz
+export PATH=$PATH:/tmp/kafka_2.12-2.3.0/bin
+rm -f /tmp/kafka_2.12-2.3.0.tgz
 
 # Download Kafka Workshop files
 git clone https://github.com/bockyanggoh/kafka-workshop.git /tmp/kafka-workshop
