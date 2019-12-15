ea60f42ecb3ef33a2af9aa28c14c86f8da6d46e0
Goh Bock Yang <bockyanggoh@gmail.com>
2019-12-15 12:46:58 +0800

    added a sleep delay to allow azure some cool time

--- azure-provision/run.sh
+++ azure-provision/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 az group create -l 'southeast asia' -n 'kafka-rg'
 az group deployment create --resource-group 'kafka-rg' --template-file azure-provision/kafka-vm-deploy.json --parameters azure-provision/kafka-vm-parameters.json
+
+sleep 5s
+
 nohup az vm run-command invoke -g 'kafka-rg' -n 'kafka1' --command-id RunShellScript --scripts @azure-provision/postinstall-scripts/install.sh &
 az vm run-command invoke -g 'kafka-rg' -n 'kafka2' --command-id RunShellScript --scripts @azure-provision/postinstall-scripts/install.sh
 
