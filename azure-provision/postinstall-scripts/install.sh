807d60e2325883abc1c01bc4fb40a4a8a4a2ac78
Goh Bock Yang <bockyanggoh@gmail.com>
2019-12-17 21:00:32 +0800

    typo

--- azure-provision/postinstall-scripts/install.sh
+++ azure-provision/postinstall-scripts/install.sh
@@ -28,7 +28,7 @@ chown kafka:user /workspace
 wget --directory-prefix=/workspace/ https://www-eu.apache.org/dist/kafka/2.3.0/kafka_2.12-2.3.0.tgz
 tar -C /workspace -xzf /workspace/kafka_2.12-2.3.0.tgz
 echo "export PATH=$PATH:/workspace/kafka_2.12-2.3.0/bin" > /etc/profile.d/kafka-path.sh
-echo "source /workspace/.bashrc" > /etc/profile.d/kafka-path.sh
+echo "source /workspace/.bashrc" >> /etc/profile.d/kafka-path.sh
 chmod 777 /etc/profile.d/kafka-path.sh
 chown root:root /etc/profile.d/kafka-path.sh
 
