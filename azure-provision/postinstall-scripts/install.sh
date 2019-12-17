ace31016c56ef87fd66d991baecea8d982b1b1c0
Goh Bock Yang <bockyanggoh@gmail.com>
2019-12-17 19:26:53 +0800

    added dead letter queue to kafka server

--- azure-provision/postinstall-scripts/install.sh
+++ azure-provision/postinstall-scripts/install.sh
@@ -11,6 +11,7 @@ sudo systemctl start docker
 sudo systemctl status docker
 sudo systemctl enable docker
 
+
 # Post install Sudo
 sudo usermod -aG docker kafka
 
@@ -19,15 +20,23 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
+# Set Home Directory
+mkdir /workspace
+chown kafka:user /workspace
+
 # Install Kafka CLI
-wget --directory-prefix=/tmp/ https://www-eu.apache.org/dist/kafka/2.3.0/kafka_2.12-2.3.0.tgz
-tar -C /tmp -xzf /tmp/kafka_2.12-2.3.0.tgz
-echo "export PATH=$PATH:/tmp/kafka_2.12-2.3.0/bin" > /etc/profile.d/kafka-path.sh
+wget --directory-prefix=/workspace/ https://www-eu.apache.org/dist/kafka/2.3.0/kafka_2.12-2.3.0.tgz
+tar -C /workspace -xzf /workspace/kafka_2.12-2.3.0.tgz
+echo "export PATH=$PATH:/workspace/kafka_2.12-2.3.0/bin" > /etc/profile.d/kafka-path.sh
 chmod 777 /etc/profile.d/kafka-path.sh
 chown root:root /etc/profile.d/kafka-path.sh
 
-rm -f /tmp/kafka_2.12-2.3.0.tgz
+rm -f /workspace/kafka_2.12-2.3.0.tgz
 
 # Download Kafka Workshop files
-git clone https://github.com/bockyanggoh/kafka-workshop.git /tmp/kafka-workshop
-find /tmp/kafka-workshop -name "*.sh" -exec chmod +x {} \;
+git clone https://github.com/bockyanggoh/kafka-workshop.git /workspace/kafka-workshop
+find /workspace/kafka-workshop -name "*.sh" -exec chmod +x {} \;
+
+# Set new home
+usermod -m -d /workspace kafka
+chown -R kafka:user /workspace/kafka-workshop
