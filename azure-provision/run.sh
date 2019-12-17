1992c2573fc9bb35658e140538bb7ad7b990653a
Goh Bock Yang <bockyanggoh@gmail.com>
2019-12-17 18:50:01 +0800

    delayed wait time for azure to be ready

--- azure-provision/run.sh
+++ azure-provision/run.sh
@@ -2,7 +2,8 @@
 az group create -l 'southeast asia' -n 'kafka-rg'
 az group deployment create --resource-group 'kafka-rg' --template-file azure-provision/kafka-vm-deploy.json --parameters azure-provision/kafka-vm-parameters.json
 
-sleep 5s
+echo "Waiting for 1 minute for Azure to complete background tasks."
+sleep 60s
 
 nohup az vm run-command invoke -g 'kafka-rg' -n 'kafka1' --command-id RunShellScript --scripts @azure-provision/postinstall-scripts/install.sh &
 az vm run-command invoke -g 'kafka-rg' -n 'kafka2' --command-id RunShellScript --scripts @azure-provision/postinstall-scripts/install.sh
