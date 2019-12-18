d2aa0022c01fd956719714ced9b06a24a75eac62
Goh Bock Yang <bockyanggoh@gmail.com>
2019-12-18 20:53:24 +0800

    added lenses, removed connect

--- docker-scripts/3-others/generate-connect-topics.sh
+++ docker-scripts/3-others/generate-connect-topics.sh
@@ -2,14 +2,15 @@ kafka-topics.sh --create --replication-factor 3 --partitions 3 --topic SuperAvro
 kafka-topics.sh --create --replication-factor 3 --partitions 3 --topic SuperEasy --zookeeper kafka1:22181
 
 kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic workshop-sql-avro-offsets --zookeeper kafka1:22181
-kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic workshop-sql-avro-offsets --zookeeper kafka1:22181
-kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic workshop-sql-avro-offsets --zookeeper kafka1:22181
+kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic workshop-sql-avro-config --zookeeper kafka1:22181
+kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic workshop-sql-avro-status --zookeeper kafka1:22181
 
 kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic workshop-loki-avro-offsets --zookeeper kafka1:22181
 kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic workshop-loki-config --zookeeper kafka1:22181
 kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic workshop-loki-status --zookeeper kafka1:22181
 
+kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic workshop-azure-offsets --zookeeper kafka1:22181
+kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic workshop-azure-config --zookeeper kafka1:22181
 kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic workshop-azure-status --zookeeper kafka1:22181
-kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic workshop-azure-status --zookeeper kafka1:22181
-kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic workshop-azure-status --zookeeper kafka1:22181
+
 kafka-topics.sh --describe --zookeeper kafka1:22181
