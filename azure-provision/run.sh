2d2c85d754e1fc1d56bc24ab2a91f982378a5423
Goh Bock Yang <bockyanggoh@gmail.com>
2019-12-25 03:10:24 +0800

    added missing vm config in deploy script, updated install.sh for database

--- azure-provision/run.sh
+++ azure-provision/run.sh
@@ -3,9 +3,11 @@ az group create -l 'southeast asia' -n 'kafka-rg'
 az group deployment create --resource-group 'kafka-rg' --template-file azure-provision/kafka-vm-deploy.json --parameters azure-provision/kafka-vm-parameters.json
 
 echo "Running Post-Installation scripts"
+nohup az vm run-command invoke -g 'kafka-rg' -n 'database' --command-id RunShellScript --scripts @azure-provision/postinstall-scripts/install.sh &
 nohup az vm run-command invoke -g 'kafka-rg' -n 'kafka1' --command-id RunShellScript --scripts @azure-provision/postinstall-scripts/install.sh &
 az vm run-command invoke -g 'kafka-rg' -n 'kafka2' --command-id RunShellScript --scripts @azure-provision/postinstall-scripts/install.sh
 
 echo "Mission Complete. Keep the following information!"
 echo "Kafka 1 Public IP: $(az vm show -d -g 'kafka-rg' -n kafka1 --query publicIps -o tsv)"
 echo "Kafka 2 Public IP: $(az vm show -d -g 'kafka-rg' -n kafka2 --query publicIps -o tsv)"
+echo "SQL Database Public IP: $(az vm show -d -g 'kafka-rg' -n kafka2 --query publicIps -o tsv), Access Port: 1433"
