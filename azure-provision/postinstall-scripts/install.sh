550851b2d0d3f184d4fa996b82a111da06714acd
Goh Bock Yang <bock@Gohs-iMac-Pro.local>
2019-12-25 01:35:49 -0800

    removed vm3 due to azure free acc restrictions

--- azure-provision/postinstall-scripts/install.sh
+++ azure-provision/postinstall-scripts/install.sh
@@ -1,60 +1,54 @@
 #!/bin/bash
 
-if [ $(hostname) == "database" ]; then
-  curl -fsSL https://get.docker.com/ | sh
-  sudo systemctl start docker
-  sudo systemctl status docker
-  sudo systemctl enable docker
-  sudo usermod -aG docker kafka
-  docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=P@ssw0rd12345' -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest
-else {
-  # Install useful tools for networking checks
-  sudo yum update -y
-  sudo yum install nc -y
-  sudo yum install git -y
-  sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
-  sudo yum install htop -y
-  sudo yum install java-11-openjdk-devel -y
-  # Install Docker
-  curl -fsSL https://get.docker.com/ | sh
-  sudo systemctl start docker
-  sudo systemctl status docker
-  sudo systemctl enable docker
+# Install useful tools for networking checks
+sudo yum update -y
+sudo yum install nc -y
+sudo yum install git -y
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
+sudo yum install htop -y
+sudo yum install java-11-openjdk-devel -y
+# Install Docker
+curl -fsSL https://get.docker.com/ | sh
+sudo systemctl start docker
+sudo systemctl status docker
+sudo systemctl enable docker
 
 
-  # Post install Sudo
-  sudo usermod -aG docker kafka
+# Post install Sudo
+sudo usermod -aG docker kafka
 
-  # Install Docker-Compose
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
-  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+# Install Docker-Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-  # Set Home Directory
-  mkdir /workspace
-  chown kafka:user /workspace
+# Set Home Directory
+mkdir /workspace
+chown kafka:user /workspace
 
-  # Install Kafka CLI
-  wget --directory-prefix=/workspace/ https://www-eu.apache.org/dist/kafka/2.3.0/kafka_2.12-2.3.0.tgz
-  tar -C /workspace -xzf /workspace/kafka_2.12-2.3.0.tgz
-  echo "export PATH=$PATH:/workspace/kafka_2.12-2.3.0/bin" > /etc/profile.d/kafka-path.sh
-  echo "source /workspace/.bashrc" >> /etc/profile.d/kafka-path.sh
-  echo "git config --global user.email kafka@workshop.com" >> /etc/profile.d/kafka-path.sh
-  echo "git config --global user.name kafka" >> /etc/profile.d/kafka-path.sh
-  chmod 777 /etc/profile.d/kafka-path.sh
-  chown root:root /etc/profile.d/kafka-path.sh
+# Install Kafka CLI
+wget --directory-prefix=/workspace/ https://www-eu.apache.org/dist/kafka/2.3.0/kafka_2.12-2.3.0.tgz
+tar -C /workspace -xzf /workspace/kafka_2.12-2.3.0.tgz
+echo "export PATH=$PATH:/workspace/kafka_2.12-2.3.0/bin" > /etc/profile.d/kafka-path.sh
+echo "source /workspace/.bashrc" >> /etc/profile.d/kafka-path.sh
+echo "git config --global user.email kafka@workshop.com" >> /etc/profile.d/kafka-path.sh
+echo "git config --global user.name kafka" >> /etc/profile.d/kafka-path.sh
+chmod 777 /etc/profile.d/kafka-path.sh
+chown root:root /etc/profile.d/kafka-path.sh
 
-  rm -f /workspace/kafka_2.12-2.3.0.tgz
+rm -f /workspace/kafka_2.12-2.3.0.tgz
 
-  # Download Kafka Workshop files
-  git clone https://github.com/bockyanggoh/kafka-workshop.git /workspace/kafka-workshop
-  find /workspace/kafka-workshop -name "*.sh" -exec chmod +x {} \;
-  cp /home/kafka/.bash* /workspace/
-  # Set new home
-  usermod -m -d /workspace kafka | true
-  chown -R kafka:kafka /workspace
+# Download Kafka Workshop files
+git clone https://github.com/bockyanggoh/kafka-workshop.git /workspace/kafka-workshop
+find /workspace/kafka-workshop -name "*.sh" -exec chmod +x {} \;
+cp /home/kafka/.bash* /workspace/
+# Set new home
+usermod -m -d /workspace kafka | true
+chown -R kafka:kafka /workspace
 
-  git config --global user.email "you@example.com"
-  git config --global user.name "Your Name"
-}
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+if [ $(hostname) == "kafka2" ]; then
+  docker-compose -f /workspace/kafka-workshop/docker-scripts/4-sql/docker-compose.yml up -d
 fi
--- azure-provision/run.sh
+++ azure-provision/run.sh
@@ -3,11 +3,18 @@ az group create -l 'southeast asia' -n 'kafka-rg'
 az group deployment create --resource-group 'kafka-rg' --template-file azure-provision/kafka-vm-deploy.json --parameters azure-provision/kafka-vm-parameters.json
 
 echo "Running Post-Installation scripts"
-nohup az vm run-command invoke -g 'kafka-rg' -n 'database' --command-id RunShellScript --scripts @azure-provision/postinstall-scripts/install.sh &
 nohup az vm run-command invoke -g 'kafka-rg' -n 'kafka1' --command-id RunShellScript --scripts @azure-provision/postinstall-scripts/install.sh &
 az vm run-command invoke -g 'kafka-rg' -n 'kafka2' --command-id RunShellScript --scripts @azure-provision/postinstall-scripts/install.sh
 
 echo "Mission Complete. Keep the following information!"
-echo "Kafka 1 Public IP: $(az vm show -d -g 'kafka-rg' -n kafka1 --query publicIps -o tsv)"
-echo "Kafka 2 Public IP: $(az vm show -d -g 'kafka-rg' -n kafka2 --query publicIps -o tsv)"
-echo "SQL Database Public IP: $(az vm show -d -g 'kafka-rg' -n kafka2 --query publicIps -o tsv), Access Port: 1433"
+kafka1=$(az vm show -d -g 'kafka-rg' -n kafka1 --query publicIps -o tsv)
+kafka2="$(az vm show -d -g 'kafka-rg' -n kafka2 --query publicIps -o tsv)"
+echo "Kafka 1 Public IP: $kafka1"
+echo "Kafka 2 Public IP: $kafka2"
+echo "SQL Database Public IP: $kafka2, Access Port: 1433"
+
+export kafka1=$kafka1
+export kafka2=$kafka2
+
+echo "Connect to Kafka1 with this command: 'ssh kafka@\$kafka1'"
+echo "Connect to Kafka2 with this command: 'ssh kafka@\$kafka2'"
